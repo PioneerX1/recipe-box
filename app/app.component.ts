8a114aac0addddc0135df296453c85cf9905527e
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-14 15:30:33 -0700

    add styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,12 +6,16 @@ import { Component } from '@angular/core';
   <div class="container">
     <div class="jumbotron">
       <h1>Angular Recipe Box</h1>
+      <h2>{{subheader}}</h2>
     </div>
     <ul>
       <li *ngFor="let currentRecipe of recipes">
-        <h4>{{currentRecipe.title}}</h4>
-        <h5>{{currentRecipe.ingredients}}</h5>
-        <h6>{{currentRecipe.directions}}</h6>
+        <div class="thumbnail">
+          <h3>{{currentRecipe.title}}</h3>
+
+          <h4>{{currentRecipe.ingredients}}</h4>
+          <h5>{{currentRecipe.directions}}</h5>
+        </div>
       </li>
     </ul>
   </div>
@@ -19,12 +23,14 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  subheader: string = 'A web app to keep track of recipes';
+  subheader: string = 'A place where foods go to die.';
   recipes: Recipe[] = [
-    new Recipe('French Onion Soup', 'onions', '1. Chop up some stanky onions and cry over the dinner you are about to have. 2. Pour that week-old nasty cream over it. 3. Serve with a side of sadness')
+    new Recipe('French Onion Soup', ['onions', 'some nasty week-old cream'], ['1. Chop up some stanky onions and cry over the dinner you are about to have.', '2. Pour that week-old nasty cream over it.', '3. Serve with a side of sadness.']),
+    new Recipe('Bean Dip', ['black beans', 'day-old, rancid queso'], ['1. Microwave these fine ingredients until they burn into a homogenous paste', '2. Serve in a small locked room with all your buddies.']),
+    new Recipe('Trash Frappe', ['warm pre-made Starbucks frap mix', 'half-congealed whipped cream'], ['1. scavenge a thrown out Starbucks cup of frappuccino from trash can.', '2. Have a buddy pour it down the top of a gutter for you.', '3. Felch the frothy mix out of the bottom of the gutter.'])
   ];
 }
 
 export class Recipe {
-  constructor(public title: string, public ingredients: string, public directions: string) { }
+  constructor(public title: string, public ingredients: string[], public directions: string[]) { }
 }
