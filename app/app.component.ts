7a2128c46703800d507e44751e3d3ea1909e065b
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-14 13:34:58 -0700

    add npm, bower, gitignore, and angular files with some basic typescript in the component, also bootstrap

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <div class="jumbotron">
+      <h1>Angular Recipe Box</h1>
+    </div>
+
+  </div>
+  `
+})
