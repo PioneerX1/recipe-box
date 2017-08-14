a792ec6342d95eb459f9b591860ec9599b94bf9d
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-14 14:13:24 -0700

    add all code for MVP on component and module ts files

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,24 @@ import { Component } from '@angular/core';
     <div class="jumbotron">
       <h1>Angular Recipe Box</h1>
     </div>
-
+    <ul>
+      <li *ngFor="let currentRecipe of recipes">
+        <h4>{{currentRecipe.title}}</h4>
+        <h5>{{currentRecipe.ingredients}}</h5>
+        <h6>{{currentRecipe.directions}}</h6>
+      </li>
+    </ul>
   </div>
   `
 })
+
+export class AppComponent {
+  subheader: string = 'A web app to keep track of recipes';
+  recipes: Recipe[] = [
+    new Recipe('French Onion Soup', 'onions', '1. Chop up some stanky onions and cry over the dinner you are about to have. 2. Pour that week-old nasty cream over it. 3. Serve with a side of sadness')
+  ];
+}
+
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public directions: string) { }
+}
