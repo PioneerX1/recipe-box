4b38975be12b7b8708f687e6b75648f0023f2ea8
Michael Dunlap and Mick Sexton <michael+mick@>
2017-08-15 10:10:26 -0700

    add edit ability for rating colors, prev was not parsing to number, so changed to accept string for rating

--- app/app.component.ts
+++ app/app.component.ts
@@ -25,8 +25,22 @@ import { Component } from '@angular/core';
             </ul>
           </div>
         </div>
+        <button (click)="editRecipe(currentRecipe)">Edit</button>
       </li>
     </ul>
+    <hr>
+    <div id="edit-recipe" *ngIf="selectedRecipe">
+      <h3>Edit Recipe: {{selectedRecipe.title}}</h3>
+      <div class="form-group">
+        <label>Enter Recipe Title: </label>
+        <input [(ngModel)]="selectedRecipe.title">
+      </div>
+      <div class="form-group">
+        <label>Enter Recipe Rating: </label>
+        <input [(ngModel)]="selectedRecipe.rating">
+      </div>
+      <button (click)="finishedEditing()">Done-zo!</button>
+    </div>
   </div>
   `
 })
@@ -34,22 +48,34 @@ import { Component } from '@angular/core';
 export class AppComponent {
   subheader: string = 'A place where foods go to die.';
   recipes: Recipe[] = [
-    new Recipe('French Onion Soup', ['onions', 'some nasty week-old cream'], ['1. Chop up some stanky onions and cry over the dinner you are about to have.', '2. Pour that week-old nasty cream over it.', '3. Serve with a side of sadness.'], 1),
-    new Recipe('Bean Dip', ['black beans', 'day-old, rancid queso'], ['1. Microwave these fine ingredients until they burn into a homogenous paste', '2. Serve in a small locked room with all your buddies.'], 2),
-    new Recipe('Trash Frappe', ['warm pre-made Starbucks frap mix', 'half-congealed whipped cream'], ['1. scavenge a thrown out Starbucks cup of frappuccino from trash can.', '2. Have a buddy pour it down the top of a gutter for you.', '3. Felch the frothy mix out of the bottom of the gutter.'], 3)
+    new Recipe('French Onion Soup', ['onions', 'some nasty week-old cream'], ['1. Chop up some stanky onions and cry over the dinner you are about to have.', '2. Pour that week-old nasty cream over it.', '3. Serve with a side of sadness.'], '1'),
+    new Recipe('Bean Dip', ['black beans', 'day-old, rancid queso'], ['1. Microwave these fine ingredients until they burn into a homogenous paste', '2. Serve in a small locked room with all your buddies.'], '2'),
+    new Recipe('Trash Frappe', ['warm pre-made Starbucks frap mix', 'half-congealed whipped cream'], ['1. scavenge a thrown out Starbucks cup of frappuccino from trash can.', '2. Have a buddy pour it down the top of a gutter for you.', '3. Felch the frothy mix out of the bottom of the gutter.'], '3')
   ];
+  selectedRecipe: Recipe = null;
+
+  editRecipe(clickedRecipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
+
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
 
   ratingColor(currentRecipe) {
-    if (currentRecipe.rating === 3) {
+    console.log(currentRecipe.rating);
+    if (currentRecipe.rating === "3") {
       return "bg-danger";
-    } else if (currentRecipe.rating === 2) {
+    } else if (currentRecipe.rating === "2") {
       return "bg-warning";
-    } else {
+    } else if (currentRecipe.rating === "1") {
       return "bg-info";
+    } else {
+      return "bg-default";
     }
   }
 }
 
 export class Recipe {
-  constructor(public title: string, public ingredients: string[], public directions: string[], public rating: number) { }
+  constructor(public title: string, public ingredients: string[], public directions: string[], public rating: string) { }
 }
